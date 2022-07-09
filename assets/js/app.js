4cac96e92ba67e4d18613bc627b6e167d5432b63
werbeth <werbeth1991pbs@gmail.com>
2022-07-09 20:21:59 -0300

    refactor: mudanças no arquivo js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,135 @@
-//função fabrica que retorna um objeto com as funcionalidades da minha calculadora
-function criandoCalculadora() {
-  return {
-    display: document.querySelector(".display"),
+//selecnanto os elementos html
+const buttonsNumber = document.querySelectorAll(".buttonNumber");
+const buttonOperators = document.querySelectorAll(".operator");
+const buttonClean = document.querySelector(".buttonClean");
+const buttonCalc = document.querySelector(".buttonCalc");
+const buttonDel = document.querySelector(".buttonDel");
+const displayPreviewElement = document.querySelector(".displayPreview");
+const displayCurrentElement = document.querySelector(".displayCurrent");
 
-    iniciar() {
-      this.btnClick();
-      this.pressEnter();
-    },
-    pressEnter() {
-      document.addEventListener("keyup", (e) => {
-        if (e.keyCode === 13) {
-          this.realizarCalculo();
-        }
-      });
-    },
-    limparDisplay() {
-      this.display.value = "";
-    },
-    deletarElemento() {
-      this.display.value = this.display.value.slice(0, -1);
-    },
-    realizarCalculo() {
-      let calculo = this.display.value;
-      try {
-        calculo = eval(calculo);
-        if (!calculo) {
-          alert("Cálculo Inválido");
-        }
-        this.display.value = calculo;
-      } catch (e) {
-        alert("Cálculo Inválido");
+//classe calculadora
+class Calculator {
+  constructor(displayPreviewElement, displayCurrentElement) {
+    this.displayPreviewElement = displayPreviewElement;
+    this.displayCurrentElement = displayCurrentElement;
+    this.clean();
+  }
+  //método que faz os calculos
+  theCalculation() {
+    let result;
+
+    const displayPreviewFloat = parseFloat(this.displayPreview);
+    const displayCurrentFloat = parseFloat(this.displayCurrent);
+
+    if (isNaN(displayPreviewFloat) || isNaN(displayCurrentFloat)) return;
+
+    switch (this.operation) {
+      case "+":
+        result = displayPreviewFloat + displayCurrentFloat;
+        break;
+      case "-":
+        result = displayPreviewFloat - displayCurrentFloat;
+        break;
+      case "*":
+        result = displayPreviewFloat * displayCurrentFloat;
+        break;
+      case "/":
+        result = displayPreviewFloat / displayCurrentFloat;
+        break;
+
+      default:
         return;
-      }
-    },
-    btnClick() {
-      document.addEventListener("click", (e) => {
-        const element = e.target;
-        if (element.classList.contains("btn-num")) {
-          this.valoresParaDisplay(element.innerText);
-        }
-        if (element.classList.contains("btn-limpar")) {
-          this.limparDisplay();
-        }
-        if (element.classList.contains("btn-delete")) {
-          this.deletarElemento();
-        }
-        if (element.classList.contains("btn-igual")) {
-          this.realizarCalculo();
-        }
+    }
+
+    this.displayCurrent = result;
+    this.operation = undefined;
+    this.displayPreview = "";
+  }
+  //método que seleciona o operador do cálculo
+  selectOperator(operation) {
+    if (this.displayCurrent === "") return;
+    if (this.displayPreview !== "") {
+      this.theCalculation();
+    }
+    this.operation = operation;
+
+    this.displayPreview = this.displayCurrent;
+    this.displayCurrent = "";
+  }
+  //métod formata o numero em casas decimais
+  formatNumber(number) {
+    const stringNumber = number.toString();
+
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
       });
-    },
-    valoresParaDisplay(valor) {
-      this.display.value += valor;
-      this.display.focus();
-    },
-  };
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+  //método que adiciona o numero digitado ao display
+  addNumberDisplay(number) {
+    if (this.displayCurrent.includes(".") && number === ".") return;
+    this.displayCurrent = `${this.displayCurrent}${number.toString()}`;
+  }
+  //método que limpa o display
+  clean() {
+    this.displayPreview = "";
+    this.displayCurrent = "";
+    this.operation = undefined;
+  }
+  //método que atualiza o display
+  updateDisplay() {
+    this.displayPreviewElement.innerText = `${this.formatNumber(
+      this.displayPreview
+    )} ${this.operation || ""}`;
+    this.displayCurrentElement.innerText = this.formatNumber(
+      this.displayCurrent
+    );
+  }
+  //método que deleta apenas um digito do display
+  delete() {
+    this.displayCurrent = this.displayCurrent.toString().slice(0, -1);
+  }
 }
 
-const calculadora = criandoCalculadora();
-calculadora.iniciar();
+//intância a classe calculadora
+const calculator = new Calculator(displayPreviewElement, displayCurrentElement);
+
+//Adicionando eventos no botões da calculadora
+for (const buttonNumber of buttonsNumber) {
+  buttonNumber.addEventListener("click", () => {
+    calculator.addNumberDisplay(buttonNumber.innerText);
+    calculator.updateDisplay();
+  });
+}
+for (const operatorButton of buttonOperators) {
+  operatorButton.addEventListener("click", () => {
+    calculator.selectOperator(operatorButton.innerText);
+    calculator.updateDisplay();
+  });
+}
+buttonClean.addEventListener("click", () => {
+  calculator.clean();
+  calculator.updateDisplay();
+});
+
+buttonCalc.addEventListener("click", () => {
+  calculator.theCalculation();
+  calculator.updateDisplay();
+});
+
+buttonDel.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
